3d1159082df9c349db9eda23558ccee760165ab5
Phenix66 <34311559+Phenix66@users.noreply.github.com>
2018-06-05 15:21:00 -0400

    Create cp.py

---
+++ Random Python/cp.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import sys
+import os.path
+
+if not len(sys.argv[0:]) == 3:
+    print "Please provide exactly 2 filenames, source and destination"
+    print "%s <source> <destination>" % (sys.argv[0])
+    sys.exit(1)
+
+src = sys.argv[1]
+dst = sys.argv[2]
+
+def mycopy():
+    with open(src, "r") as s:
+        with open(dst, "w+") as d:
+            size = 100
+            while size == 100:
+                data = s.read(100)
+                size = len(data)
+                d.write(data)
+
+try:
+    confirm = "n"
+    if os.path.isfile(dst):
+        confirm = raw_input("Destination file exists, do you want to overwrite? (y/N) ")
+        if confirm.lower() == "y" or confirm.lower() == "yes":
+            mycopy()
+        elif confirm.lower() == "n" or confirm.lower() == "no":
+            print "Skipping copy"
+        else:
+            print "Invalid overwrite option specified, skipping copy"
+    else:        
+        mycopy()
+except IOError, err:
+    print err
+    sys.exit(1)
