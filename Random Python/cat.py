df3f10614ccc6756909a373555b33dabd012ce0f
Phenix66 <34311559+Phenix66@users.noreply.github.com>
2018-06-05 15:24:07 -0400

    Create cat.py

---
+++ Random Python/cat.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import sys
+
+lstFiles = []
+
+if not len(sys.argv[0:]) < 2:
+    for x in sys.argv[1:]:
+        lstFiles.append(x)
+
+    for n in lstFiles:
+        try:
+            with open(n, "r") as f:
+                size = 100
+                while size == 100:
+                    data = f.read(100)
+                    size = len(data)
+                    sys.stdout.write(data)
+        except IOError, err:
+            print err
+
+    print " "
+else:
+    print "Please provide at least one file name"
