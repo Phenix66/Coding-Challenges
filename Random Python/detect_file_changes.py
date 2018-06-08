209bc19e298810cb335f4f78662629f2313e7f79
Phenix66 <34311559+Phenix66@users.noreply.github.com>
2018-06-08 09:55:51 -0400

    Update detect_file_changes.py

--- Random Python/detect_file_changes.py
+++ Random Python/detect_file_changes.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 import os
 import sys
-
-dictHashes = {}
+from subprocess import Popen, PIPE, STDOUT
 
 def hash(fullpath):
     cmd = "md5sum %s" % (fullpath)
-    f = os.popen(cmd)
-    line = f.read().strip()
-    (md5, fullname) = line.split()
-    return md5
+    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    output = p.stdout.read()
+    if "No such file or directory" in output:
+        # md5sum returning this error indicates a file was deleted
+        return "null"
+    else:
+        (md5, fullname) = output.split()
+        return md5
 
-def load_hashes(files):
+def load_hashes(mydir, dictHashes):
     # Loads hashes if they exist
     try:
         with open("./hashes.txt", "r") as f:
@@ -19,46 +22,20 @@ def load_hashes(files):
             for line in lines:
                 (val, key) = line.strip().split()
                 dictHashes[key] = val
-    except:
-        gen_hashes(files)
+    except IOError:
+        dictHashes = gen_hashes(mydir)
+    return dictHashes
 
-def write_hashes():
+def write_hashes(dictHashes):
     # Updates (or creates) hash file in current directory
     with open("./hashes.txt", "w") as f:
         for key, val in dictHashes.iteritems():
             line = "%s %s\n" % (val, key)
             f.write(line)
 
-def gen_hashes(files):
-    # Creates starting values
-    for filename in files:
-        dictHashes[filename] = hash(filename)
-
-def monitor(files):
-    # Continually check for changes
-    while True:
-        for filename in files:
-            try:
-                currentHash = hash(filename)
-                if filename in dictHashes:
-                    if currentHash != dictHashes[filename]:
-                        print "%s has changed!!" % (filename)
-                        dictHashes[filename] = currentHash
-                else:
-                    print "New file created!! %s" % (filename)
-                    dictHashes[filename] = currentHash
-            except ValueError:
-                pass
-
-if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        mydir = os.path.abspath(".")
-    elif len(sys.argv) == 2:
-        mydir = os.path.abspath(sys.argv[1])
-    else:
-        print "Too many arguments provided.\n%s [<directory>]" % (sys.argv[0])
-        sys.exit(1)
-
+def gen_hashes(mydir):
+    # Creates starting values if hash file did not exist
+    dictHashes = {}
     files = []
     filenames = os.listdir(mydir)
 
@@ -67,13 +44,57 @@ if __name__ == "__main__":
         if os.path.isfile(fullpath):
             files.append(fullpath)
 
+    for filename in files:
+        dictHashes[filename] = hash(filename)
+    return dictHashes
+
+def monitor(dictHashes):
+    # Continually check for changes
+    while True:
+        files = []
+        filenames = os.listdir(mydir)
+
+        for file in filenames:
+            fullpath = "%s/%s" % (mydir, file)
+            if os.path.isfile(fullpath):
+                files.append(fullpath)
+
+        for filename in files:
+            try:
+                currentHash = hash(filename)
+                if filename in dictHashes:
+                    if currentHash == "null":
+                        print "%s has been deleted!" % (filename)
+                        del dictHashes[filename]
+                    else:
+                        if currentHash != dictHashes[filename]:
+                            print "%s has changed!!" % (filename)
+                            dictHashes[filename] = currentHash
+                else:
+                    print "New file created!! %s" % (filename)
+                    dictHashes[filename] = currentHash
+            except ValueError:
+                pass
+
+if __name__ == "__main__":
+    # Allows user to specify directory to monitor. Defaults to current directory.
+    if len(sys.argv) == 1:
+        mydir = os.path.abspath(".")
+    elif len(sys.argv) == 2:
+        mydir = os.path.abspath(sys.argv[1])
+    else:
+        print "Too many arguments provided.\n%s [<directory>]" % (sys.argv[0])
+        sys.exit(1)
+
+    dictHashes = {}
+
     try:
-        load_hashes(files)
-        monitor(files)
+        dictHashes = load_hashes(mydir, dictHashes)
+        monitor(dictHashes)
     except KeyboardInterrupt:
         confirm = raw_input("\nWrite changed hashes to disk? (Y/n) ")
         if confirm.lower == "n" or confirm.lower == "no":
             sys.exit(0)
         else:
-            write_hashes()
+            write_hashes(dictHashes)
             sys.exit(0)
