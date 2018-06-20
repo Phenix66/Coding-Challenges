ae481b568a9ede91244556e5871f8aa9706aa1f9
Phenix66 <34311559+Phenix66@users.noreply.github.com>
2018-06-20 14:13:52 -0400

    Create hex_mafs.py

---
+++ CodeWarz/hex_mafs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+
+file = sys.argv[1]
+lines = []
+with open(file, "r") as f:
+    lines = f.readlines()
+
+for line in lines:
+    nums = line.split()
+    sum = 0
+    for n in nums:
+        sum = sum + int(n, 16)
+    print(sum)
