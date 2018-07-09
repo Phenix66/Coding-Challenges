cde32efa8b1465e1bd259a299dead919c4daef0f
Phenix66 <34311559+Phenix66@users.noreply.github.com>
2018-07-09 12:43:17 -0400

    Added comments

--- Random Python/Socket Games/py_hilo_solve.py
+++ Random Python/Socket Games/py_hilo_solve.py
@@ -12,15 +12,19 @@ s = socket.socket()
 s.connect((IP, PORT))
 
 counter = 1
+# Receive initial challenge, discard
 s.recv(4096)
+# Send beginning guess of 500
 s.send(str(guess))
 while counter < 10:
     response = s.recv(4096)
     print response
     if "Higher..." in response:
+        # Narrows range for guessing, then divides remaining range in half to determine new guess
         minnum = guess
         guess = guess + ((maxnum - minnum)/2)
     elif "Lower..." in response:
+        # Same as above, but decreases instead of increasing
         maxnum = guess
         guess = guess - ((maxnum - minnum)/2)
     s.send(str(guess))
