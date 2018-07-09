ed01a7c2c109dfd950f6fb41be62138d8cf2ff88
Phenix66 <34311559+Phenix66@users.noreply.github.com>
2018-07-09 12:36:04 -0400

    Create captcha_answer.py

---
+++ Random Python/Socket Games/captcha_answer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import socket
+import os
+from modules import word2number as w2n
+from modules import num2words as n2w
+
+IP = "127.0.0.1"
+PORT = 12345
+
+s = socket.socket()
+s.connect((IP, PORT))
+
+def recv_all():
+    received_data = s.recv(1024)
+    data = (received_data.split())[-6:]
+    for x in data:
+        print x,
+    print " "
+    return data
+
+def send_all(data):
+    s.sendall(data)
+    print data + "\n"
+    return
+
+while True:
+    data = recv_all()
+    try:
+        numword1 = data[3]
+        numword2 = data[5].strip("?")
+        num1 = w2n.word_to_num(numword1)
+        num2 = w2n.word_to_num(numword2)
+        sum = num1 + num2
+        sumword = n2w.int2word(sum)
+        send_all(sumword)
+    except ValueError:
+        break
+    except IndexError:
+        if data == []:
+            break
